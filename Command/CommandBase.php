a4c2019d301d4eb440a764ae7ffd15ac330d8778
Jo Carter <github@jocarter.co.uk>
2020-05-05 14:41:22 +0100

    Fix symfony 4 deprecations

--- Command/CommandBase.php
+++ Command/CommandBase.php
@@ -4,33 +4,30 @@ namespace MyBuilder\Bundle\CronosBundle\Command;
 
 use MyBuilder\Bundle\CronosBundle\Exporter\AnnotationCronExporter;
 use MyBuilder\Cronos\Formatter\Cron;
-use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerAwareTrait;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
-class CommandBase extends ContainerAwareCommand
+class CommandBase extends Command implements ContainerAwareInterface
 {
-    protected function addServerOption()
+    use ContainerAwareTrait;
+
+    protected function addServerOption(): void
     {
         $this
             ->addOption('server', null, InputOption::VALUE_REQUIRED, 'Only include cron jobs for the specified server', AnnotationCronExporter::ALL_SERVERS);
     }
 
-    /**
-     * Configure cron export
-     *
-     * @param InputInterface  $input
-     * @param OutputInterface $output
-     *
-     * @return Cron
-     */
-    protected function configureCronExport(InputInterface $input, OutputInterface $output)
+    protected function configureCronExport(InputInterface $input, OutputInterface $output): Cron
     {
-        $options = array(
+        $options = [
             'serverName' => $input->getOption('server'),
-            'environment' => $input->getOption('env')
-        );
+            'environment' => $input->getOption('env'),
+        ];
 
         $output->writeln(sprintf('Server <comment>%s</comment>', $options['serverName']));
         $cron = $this->exportCron($options);
@@ -39,9 +36,30 @@ class CommandBase extends ContainerAwareCommand
         return $cron;
     }
 
-    private function exportCron($options)
+    /** @throws \LogicException */
+    protected function getContainer(): ContainerInterface
+    {
+        if (null === $this->container) {
+            $application = $this->getApplication();
+
+            if (null === $application) {
+                throw new \LogicException('The container cannot be retrieved as the application instance is not yet set.');
+            }
+
+            $this->container = $application->getKernel()->getContainer();
+
+            if (null === $this->container) {
+                throw new \LogicException('The container cannot be retrieved as the kernel has shut down.');
+            }
+        }
+
+        return $this->container;
+    }
+
+    private function exportCron($options): Cron
     {
         $commands = $this->getApplication()->all();
+        /** @var AnnotationCronExporter $exporter */
         $exporter = $this->getContainer()->get('mybuilder.cronos_bundle.annotation_cron_exporter');
 
         return $exporter->export($commands, $options);

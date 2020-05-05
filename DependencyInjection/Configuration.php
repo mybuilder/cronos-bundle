af1b123e57f3ef896169cc3a83fa16320fde824c
Jo Carter <github@jocarter.co.uk>
2020-05-05 15:37:28 +0100

    Specify reason for TreeBuilder change

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -10,9 +10,11 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder(): TreeBuilder
     {
         if (method_exists(TreeBuilder::class, 'getRootNode')) {
+            // Symfony 4
             $treeBuilder = new TreeBuilder('my_builder_cronos');
             $rootNode = $treeBuilder->getRootNode();
         } else {
+            // Symfony 3
             $treeBuilder = new TreeBuilder();
             $rootNode = $treeBuilder->root('my_builder_cronos');
         }

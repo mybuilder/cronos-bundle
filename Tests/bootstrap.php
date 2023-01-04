6f06ffd4ed8f5dd67175a6ce605513765872578e
Christopher Georg <christopher.georg@sr-travel.de>
2023-01-04 14:19:11 +0100

    feat: drop support of php 7, migrate phpunit.xml.dist to v. 9 format, allow doctrine/annotations v2, allow mybuilder/cronos v4

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -2,11 +2,17 @@
 
 error_reporting(error_reporting() & ~E_USER_DEPRECATED);
 
+use Doctrine\Common\Annotations\AnnotationRegistry;
+
 // Composer
 if (file_exists(__DIR__ . '/../vendor/autoload.php')) {
     $loader = require __DIR__ . '/../vendor/autoload.php';
+
+    if(method_exists(AnnotationRegistry::class, 'registerLoader'))
+    {AnnotationRegistry::registerLoader('class_exists');}
 
     return $loader;
 }
 
 throw new \RuntimeException('Could not find vendor/autoload.php, make sure you ran composer.');
+

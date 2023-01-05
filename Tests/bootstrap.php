fbb719825f57adafc22ab3189b172d03b571d436
chris <chris@sky-scripts.de>
2023-01-05 20:15:22 +0100

    Update Tests/bootstrap.php
    Co-authored-by: Max Baldanza <massimiliano.baldanza@instapro.com>

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -8,8 +8,9 @@ use Doctrine\Common\Annotations\AnnotationRegistry;
 if (file_exists(__DIR__ . '/../vendor/autoload.php')) {
     $loader = require __DIR__ . '/../vendor/autoload.php';
 
-    if(method_exists(AnnotationRegistry::class, 'registerLoader'))
-    {AnnotationRegistry::registerLoader('class_exists');}
+    if (method_exists(AnnotationRegistry::class, 'registerLoader')) {
+        AnnotationRegistry::registerLoader('class_exists');
+    }
 
     return $loader;
 }
